11811661e16ee38228640ca061fe894b8d181190
イ・ジュンホ <leejunho@ijunhonoMacBook-Pro.local>
2017-11-22 01:04:46 +0900

    modify SearchController

--- ums/src/main/java/jp/co/asia/archive/ums/app/SearchController.java
+++ ums/src/main/java/jp/co/asia/archive/ums/app/SearchController.java
@@ -14,15 +14,15 @@ public class SearchController {
    *
    * @return 検索フォーム画面名
    */
-  @RequestMapping(value = "search", method = RequestMethod.GET)
-  public ModelAndView searchForm(Form form) { //MEMO 画面のformタグ中の要素をControllerで受け取ります。
-
+  @RequestMapping(value = "search", method = RequestMethod.GET, params = "form")
+  public ModelAndView searchForm(SearchForm searchForm) { //MEMO 画面のformタグ中の要素をControllerで受け取ります。
+	// FIXME 画面遷移実験用(top画面の検索ボタン作製後削除)  http://localhost:8080/ums/user/search?form
     //TODO form model関連実装
 
     String checkForm = "これ出たらform成功";
 
     ModelAndView mav = new ModelAndView();
-    mav.addObject("form", form);
+    mav.addObject("form", searchForm);
     mav.addObject("checkForm", checkForm);
     mav.setViewName("user/searchForm");
     return mav;
@@ -33,11 +33,10 @@ public class SearchController {
    *
    * @return ModelAndView //MEMO 使用者(プログラマー)がソース見て分かるような部分は、このくらいで書いていい
    */
-  //データ渡し実験用URL: http://localhost:8080/ums/search/list?userId=a&userName=a&birthDay=a&address=a&telNum=a&roles=a&state=a
   @RequestMapping(value = "search", method = RequestMethod.GET) //TODO 後でPOSTに変更予定
   public ModelAndView searchList() {
-
-    //TODO list model関連実装
+    // FIXME データ渡し実験用(searchForm.jsp作成後削除) URL: http://localhost:8080/ums/user/search?userId=a&userName=a&birthDay=a&address=a&telNum=a&roles=a&state=a
+    // TODO list model関連実装
 
     String checkList = "これ出たらlist成功";
 
